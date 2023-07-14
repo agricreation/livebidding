744a3e392509d9e5a0682655e9a32f4b542256ef
moovendhanagri <72656436+moovendhanagri@users.noreply.github.com>
2023-07-14 17:18:31 +0530

    setup

---
+++ livebid/src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <h1 className='text-center fw-bold text-primary'>welome to libebidding home page</h1>
+    </div>
+  );
+}
+
+export default App;
